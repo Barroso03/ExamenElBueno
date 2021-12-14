d662766c33d45399d73eacaf40876fdb4c28de48
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-12-14 10:56:36 +0100

    definimos una función para declarar el ganador del juego y que nos diga su puntuación

--- The_minion_game.py
+++ The_minion_game.py
@@ -4,4 +4,17 @@ def minion_game(s):
     puntuacionKevin=0
     vocales= 'AEIOU'
     s=s.upper()
-    
+    for i in range(len(s)):
+        if s[i] not in vocales:
+            puntuacionStuart=puntuacionStuart+(len(s)-i)
+        else:
+            puntuacionKevin=puntuacionKevin+(len(s)-i)
+    if puntuacionStuart>puntuacionKevin:
+        print("Stuart ha ganado, puntuación:", puntuacionStuart)
+    elif puntuacionStuart<puntuacionKevin:
+        print("Kevin ha ganado, puntuación:", puntuacionKevin)
+    else:
+        print("Empate")
+
+        
+
